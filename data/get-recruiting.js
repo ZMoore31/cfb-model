d0faeebe301cb9b92db79be440acf700210a667e
Zack Moore <ZMoore31@gmail.com>
2018-09-18 22:07:18 -0600

    move recruiting script to different file

---
+++ data/get-recruiting.js
@@ -0,0 +1,47 @@
+const cfb = require("cfb-data");
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+const { Recruiting } = require("./database/schema");
+const database = require("./database/database");
+
+mongoose.connect(
+  database(),
+  { useNewUrlParser: true }
+);
+
+var years = [];
+for (var i = 2001; i <= 2019; i++) {
+  years.push(i);
+}
+
+const recruiting = async (years, callback) => {
+  years.forEach(year => {
+    for (i = 1; i <= 6; i++) {
+      cfb.recruiting.getSchoolRankings(year, i).then(result => {
+        result.map(obj => {
+          const newRecruiting = Recruiting({
+            year: year,
+            rank: obj.rank === "N/A" ? "" : obj.rank,
+            school: obj.school,
+            totalCommits: obj.totalCommits,
+            fiveStars: obj.fiveStars,
+            fourStars: obj.fourStars,
+            threeStars: obj.threeStars,
+            averageRating: obj.averageRating,
+            points: obj.points
+          }).save((err, data) => {
+            if (err) return res.send(err);
+            console.log("Success");
+          });
+        });
+      });
+    }
+  });
+};
+
+recruiting(years);
+
+setTimeout(() => {
+  mongoose.disconnect();
+}, 60 * 1000);
