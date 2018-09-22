bc9951fd7b26fe49b2f8126d0fb7ae43206a6460
Zack Moore <ZMoore31@gmail.com>
2018-09-21 20:45:33 -0600

    get team info and update recruiting school names

--- python-data/get-team-info.py
+++ python-data/get-team-info.py
@@ -1,42 +1,43 @@
-from pymongo import MongoClient
 import json
 import sys
 import time
 import requests
 import pandas as pd
 import numpy as np
+import mysql.connector
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DATABASE_USER = os.environ.get("DATABASE_USER")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+
+engine = create_engine('mysql+mysqlconnector://'+DATABASE_USER+':'+DATABASE_PASSWORD+'@127.0.0.1/CFB', echo=False)
 
 sys.path.append('./services')
 from team import getTeamInfo
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['CFB']
-ratings = db.ratings
+team_list = list(pd.read_sql('Select distinct teamID from ratings', engine)['teamID'])
 
-df =  pd.DataFrame(list(ratings.find({})))
-df = list(pd.unique(df['teamID']))
-print(df)
+def store_team_info(id):
+    info = getTeamInfo(id)
+    return pd.DataFrame.from_dict({
+        'teamID': [info['team']['id']],
+        'schoolName': [info['team']['location']],
+        'teamName': [info['team']['name']],
+        'abbreviation': [info['team']['abbreviation']],
+        'displayName': [info['team']['displayName']],
+        'shortDisplayName': [info['team']['shortDisplayName']],
+        'color': [info['team']['color']],
+        'altColor': [info['team']['alternateColor']] if 'alternateColor' in info['team'] else [''] ,
+        'logo': [info['team']['logos'][0]['href']] if 'logos' in info['team'] else [''] ,
+    })
 
-def calc_massey(year):
-    df =  pd.DataFrame(list(games.find({'year': year})))
-    df['pointsDiff'] = pd.to_numeric(df['pointsFor']) - pd.to_numeric(df['pointsAgainst'])
-    totalPointsDiff = df.groupby(['teamID']).sum()['pointsDiff']
-    totalPointsDiff[-1] = 0
-    gameCount = df.groupby(['teamID']).size()
-    teamList = pd.unique(df['teamID'])
-    numberTeams = len(teamList)
-    identity = np.identity(numberTeams)
-    T = gameCount.values * identity
-    T = pd.DataFrame(T)
-    T.columns = gameCount.index.values
-    T.index = gameCount.index.values
-    gameCombos = pd.concat([pd.DataFrame(list(df.groupby(['teamID','opposingTeamID']).size().index.values)),pd.DataFrame(df.groupby(['teamID','opposingTeamID']).size()).reset_index(drop=True)], axis=1)
-    for game in gameCombos.values:
-        T[game[0]][game[1]] = -game[2]
-    T.iloc[-1] = 1
-    ratings = pd.DataFrame(np.linalg.inv(T.values).dot(totalPointsDiff.values))
-    ratings.index = gameCount.index.values
-    ratings.columns = ['Rating']
-    ratings['Year'] = year
-    ratings['teamID'] = gameCount.index.values
-    return ratings[['teamID','Year','Rating']]
+for team in team_list:
+    print(team)
+    store_team_info(team).to_sql('team_info', con=engine, if_exists='append')
+    time.sleep(1)
---
+++ python-data/recruiting_scrubs.sql
@@ -0,0 +1,71 @@
+update recruiting_rankings
+set school = 'BYU'
+where school = 'Brigham Young';
+
+update recruiting_rankings
+set school = 'UMass'
+where school = 'Massachusetts';
+
+update recruiting_rankings
+set school = 'NC State'
+where school = 'N.C. State';
+
+update recruiting_rankings
+set school = 'McNeese'
+where school = 'McNeese State';
+
+update recruiting_rankings
+set school = 'Middle Tennessee'
+where school = 'Middle Tennessee State';
+
+update recruiting_rankings
+set school = 'UL Monroe'
+where school = 'Louisiana-Monroe';
+
+update recruiting_rankings
+set school = 'Nicholls'
+where school = 'Nicholls State';
+
+update recruiting_rankings
+set school = 'Southern Mississippi'
+where school = 'Southern Miss';
+
+update recruiting_rankings
+set school = 'UT Martin'
+where school = 'Tennessee-Martin';
+
+update recruiting_rankings
+set school = 'UConn'
+where school = 'Connecticut';
+
+update recruiting_rankings
+set school = 'South Florida'
+where school = 'USF';
+
+update recruiting_rankings
+set school = 'Hawai\'i'
+where school = 'Hawaii';
+
+update recruiting_rankings
+set school = 'Florida Intl'
+where school = 'FIU';
+
+update recruiting_rankings
+set school = 'Grambling'
+where school = 'Grambling State';
+
+update recruiting_rankings
+set school = 'Central Connecticut'
+where school = 'Central Connecticut State';
+
+update recruiting_rankings
+set school = 'Prairie View'
+where school = 'Prairie View A&M';
+
+update recruiting_rankings
+set school = 'Presbyterian College'
+where school = 'Presbyterian';
+
+update recruiting_rankings
+set school = 'Texas A&M-Commerce'
+where school = 'Texas A&M Commerce';
